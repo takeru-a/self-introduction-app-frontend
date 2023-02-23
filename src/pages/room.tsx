f5eaf259e59e8c78dcbe4bfacb7565325a58ab59
takeru-a <pypy20191022@gmail.com>
2023-02-24 08:24:30 +0900

    追加ユーザの表示機能の実装

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -2,20 +2,54 @@ import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import type { NextPage } from "next";
-import { useQuery, useMutation } from "@apollo/client";
-import { GetLoginedUserDocument } from "../graphql/dist/client";
-import { GetLoginedUserQuery } from "../graphql/dist/client";
-import { useState } from 'react';
+import { useQuery, useSubscription} from "@apollo/client";
+import { GetLoginedUserDocument, GetRoomDocument, AddPlayersDocument } from "../graphql/dist/client";
+import { GetLoginedUserQuery, GetRoomQuery, AddPlayersSubscription } from "../graphql/dist/client";
 import { useRouter } from 'next/router';
 
 const RoomPage:NextPage = () =>{
     const router = useRouter();
-    const token = router.query.c;
+    const host = "localhost:3000"
     const { data }  = useQuery<GetLoginedUserQuery>(GetLoginedUserDocument)
+    if(data?.loginedUser.code === 401){
+        router.push("/")
+    }
+    const room = useQuery<GetRoomQuery>(GetRoomDocument, {variables: {id: data?.loginedUser.roomId}})
+    const players = useSubscription<AddPlayersSubscription>(AddPlayersDocument, {variables:{token: data?.loginedUser.roomToken, userId: data?.loginedUser.userId}})
+
+    const copylink = () => {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText( host + "/?c="+ data?.loginedUser.roomToken);
+      }
+    }
     return (
     <>
-    <p>room: {token}</p>
-    logined: { data?.loginedUser.name }
+      <div style={{ margin: "0 auto", width: "1000px" }}>
+      <p>AddPlayers</p>
+      {
+      players.data?.changeRoom.players.map((player) => (
+        <div key={player.id}>
+          <p>{player.name}</p>
+        </div>
+         ))}
+        <p>Players</p>
+         {
+      room.data?.room.players.map((player) => (
+        <div key={player.id}>
+          <p>{player.name}</p>
+        </div>
+         ))}
+      </div>
+    <p>token: {data?.loginedUser.roomToken}</p>
+    <p>room_id: {data?.loginedUser.roomId}</p>
+    <p>logined: { data?.loginedUser.name }</p>
+    <p>code: {data?.loginedUser.code}</p>
+    <p>user_id: {data?.loginedUser.userId}</p>
+    <div>
+    <button onClick={copylink}>
+      Copy Link
+    </button>
+  </div>
     </>
     );
 }
